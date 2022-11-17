860be150d0297fc5b61441242b83e99646246210
Olga Tananova <tananovao@yandex.ru>
2022-11-17 12:41:10 -0800

    fix: fixed app launching in production mode

--- middlewares/auth.ts
+++ middlewares/auth.ts
@@ -1,13 +1,16 @@
 import jwt from 'jsonwebtoken';
 import UnauthorizedError from "../errors/UnauthorizedError";
-import {unauthorizedMessage} from "../constants";
+import {publicKey, unauthorizedMessage} from "../constants";
 import {Response, Request, NextFunction} from "express";
+
+const {NODE_ENV, JWT_SECRET} = process.env;
+const secretKey: string = NODE_ENV === 'production' && JWT_SECRET || publicKey;
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.cookies.jwt;
     if (!token ) {
         return next(new UnauthorizedError(unauthorizedMessage));
     }
-    req.user = jwt.verify(token, 'some-secret-key');
+    req.user = jwt.verify(token, secretKey);
     next();
 }
